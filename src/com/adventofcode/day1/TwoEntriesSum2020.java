2d40bfa5f12b2f1c00a7adc3e1f0ede017c404e7
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-02 10:16:32 +0100

    Since the list is not big and there are no additional restrictions, a map is used to store all values and then its values are iterated to look for those whose sum is 2020.

--- src/com/adventofcode/day1/TwoEntriesSum2020.java
+++ src/com/adventofcode/day1/TwoEntriesSum2020.java
@@ -3,31 +3,38 @@ package com.adventofcode.day1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoEntriesSum2020 {
     private static final String PUZZLE_INPUT = "resources/input.txt";
+    private static final Integer TARGET = 2020;
 
     public static void main (String [] args) {
-        List<Integer> numbers = readPuzzleInput();
-        for (Integer number : numbers) {
-            System.out.println(number);
+        Map<Integer, Integer> numbers = readPuzzleInput();
+        for (Integer number : numbers.values()) {
+            if (numbers.containsKey(TARGET - number)) {
+                System.out.println(number + " + " + (TARGET - number) + " = " + TARGET);
+                System.out.println(number + " * " + (TARGET - number) + " = " + (number * (TARGET - number)));
+                break;
+            }
         }
 
     }
 
-    private static List<Integer> readPuzzleInput() {
-        List<Integer> numbers = new ArrayList<>();
+    private static Map<Integer, Integer> readPuzzleInput() {
+        Map<Integer, Integer> numbers = new HashMap<>();
         String file = PUZZLE_INPUT;
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
-                numbers.add(Integer.valueOf(line));
+                numbers.put(Integer.valueOf(line), Integer.valueOf(line));
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
         return numbers;
     }
+
+
 }
