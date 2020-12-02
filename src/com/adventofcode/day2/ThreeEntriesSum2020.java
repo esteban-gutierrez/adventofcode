4835d91a69f210d3e5e2e8b7506d045b7fe273b0
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-02 11:59:42 +0100

    Solution for day 2: apply solution for day 1... twice.

--- src/com/adventofcode/day2/ThreeEntriesSum2020.java
+++ src/com/adventofcode/day2/ThreeEntriesSum2020.java
@@ -1,4 +1,33 @@
 package com.adventofcode.day2;
 
+import com.adventofcode.util.CommonUtils;
+
+import java.util.Map;
+
 public class ThreeEntriesSum2020 {
+    private static final Integer TARGET = 2020;
+
+    public static void main (String [] args) {
+        Map<Integer, Integer> numbers = CommonUtils.readPuzzleInput();
+        Integer numberTwo, numberThree;
+        for (Integer numberOne : numbers.keySet()) {
+            Integer nextTarget = TARGET - numberOne;
+            numberTwo = findNumber(numbers, nextTarget);
+            if (numberTwo != null) {
+                numberThree = nextTarget - numberTwo;
+                System.out.println(numberOne + ", " + numberTwo + ", " + numberThree);
+                System.out.println(numberOne + " * " + numberTwo + " * " + numberThree + " = " + (numberOne * numberTwo * numberThree));
+                break;
+            }
+        }
+    }
+
+    private static Integer findNumber(Map<Integer, Integer> numbers, Integer target) {
+        for (Integer number : numbers.values()) {
+            if (numbers.containsKey(target - number)) {
+                return (target - number);
+            }
+        }
+        return null;
+    }
 }
