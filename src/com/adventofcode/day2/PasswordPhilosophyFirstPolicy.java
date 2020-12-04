996c1ed266f890d07af2cae852c189018b4ed9e3
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-04 01:35:09 +0100

    Class for first challenge of day 2 renamed accordingly.

--- src/com/adventofcode/day2/PasswordPhilosophyFirstPolicy.java
+++ src/com/adventofcode/day2/PasswordPhilosophyFirstPolicy.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class PasswordPhilosophy {
+public class PasswordPhilosophyFirstPolicy {
     private static final String PASSWORDS = "resources/passwords_day_2.txt";
 
     public static void main (String [] args) {
