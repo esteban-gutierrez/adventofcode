435c6c42c13688ca6a06b0ab12aba3103ee60c0a
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-04 11:54:50 +0100

    Strict validation implemented and 'toString' method updated accordingly.

--- src/com/adventofcode/day4/PassportBean.java
+++ src/com/adventofcode/day4/PassportBean.java
@@ -108,9 +108,63 @@ public class PassportBean {
                 && hcl != null && ecl != null && pid != null; // it is accepted that cid might not be set
     }
 
+    private boolean isByrValid() {
+        return byr.length() == 4
+                && Integer.valueOf(byr) >= 1920
+                && Integer.valueOf(byr) <= 2002;
+    }
+
+    private boolean isIyrValid() {
+        return iyr.length() == 4
+                && Integer.valueOf(iyr) >= 2010
+                && Integer.valueOf(iyr) <= 2020;
+    }
+
+    private boolean isEyrValid() {
+        return eyr.length() == 4
+                && Integer.valueOf(eyr) >= 2020
+                && Integer.valueOf(eyr) <= 2030;
+    }
+
+    private boolean isHgtValid() {
+        String [] heightData = hgt.split("cm");
+        if (heightData.length == 1) {
+            heightData = hgt.split("in");
+            if (heightData.length == 2) {
+                return Integer.valueOf(heightData[0]) >= 59 &&  Integer.valueOf(heightData[0]) <= 76;
+            }
+        } else {
+            return Integer.valueOf(heightData[0]) >= 150 &&  Integer.valueOf(heightData[0]) <= 193;
+        }
+        return false;
+    }
+
+    private boolean isHclValid() {
+        String [] hairColorData = hcl.split("#");
+        if (hairColorData.length == 2) {
+            return hairColorData[1].matches("^([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$");
+        }
+        return false;
+    }
+
+    private boolean isEclValid() {
+        return ecl.equals("amb") || ecl.equals("blu") || ecl.equals("brn")
+                || ecl.equals("gry") || ecl.equals("grn") || ecl.equals("hzl") || ecl.equals("oth");
+    }
+
+    private boolean isPidValid() {
+        return pid.matches("\\\\d+") && pid.length() == 9;
+    }
+
+    public boolean isValid() {
+        return areRequiredFieldsPopulated() && isByrValid() && isEclValid() && isEyrValid()
+                && isHclValid() && isHgtValid() && isIyrValid() && isPidValid();
+    }
+
     public String toString() {
         return "byr=" + byr + ", iyr=" + iyr + ", eyr=" + eyr + ", hgt=" + hgt
                 + ", hcl=" + hcl + ", ecl=" + ecl + ", pid=" + pid + ", cid=" + cid
-                + "\t Is valid=" + areRequiredFieldsPopulated();
+                + "\t Are required fields populated?=" + areRequiredFieldsPopulated()
+                + "\t Is valid?=" + isValid();
     }
 }
