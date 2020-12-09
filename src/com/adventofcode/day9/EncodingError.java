0002dea0b2023b5ba69054debe91e9cfcad419b0
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-09 22:14:01 +0100

    Solution of first challenge of day 9.

--- src/com/adventofcode/day9/EncodingError.java
+++ src/com/adventofcode/day9/EncodingError.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 
 public class EncodingError {
     private static final String ENCRYPTED_DATA = "resources/encrypted_data_day_9.txt";
+    private static final int AMOUNT_OF_NUMBERS = 25;
+
     private static long[] encryptedData;
-    private static int[] previousNumbers = new int[25];
+    private static long[] previousNumbers = new long[AMOUNT_OF_NUMBERS];
 
     public static void main(String[] args) {
         encryptedData = new long[countNumbers()];
@@ -18,6 +20,15 @@ public class EncodingError {
                 encryptedData[i] = Long.valueOf(line);
                 i++;
             }
+
+            boolean pairFound ;
+            i = 0;
+            do {
+                updateNumbers(i);
+                pairFound = anyPairSumsTarget(encryptedData[i + AMOUNT_OF_NUMBERS]);
+                i++;
+            } while ((i < encryptedData.length) && pairFound);
+
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
@@ -34,5 +45,23 @@ public class EncodingError {
             ioe.printStackTrace();
         }
         return numbers;
+    }
+
+    private static void updateNumbers(int position) {
+        for (int i = 0; i < AMOUNT_OF_NUMBERS; i++) {
+            previousNumbers[i] = encryptedData[i + position];
+        }
+    }
+
+    private static boolean anyPairSumsTarget(long target) {
+        for (int i = 0; i < previousNumbers.length - 1; i++) {
+            for (int j = 1; j < previousNumbers.length; j++) {
+                if ((i != j) && (previousNumbers[i] + previousNumbers[j] == target)) {
+                    return true;
+                }
+            }
+        }
+        System.out.println("This number cannot be obtained by adding 2 of the previous 25 numbers: " + target);
+        return false;
     }
 }
