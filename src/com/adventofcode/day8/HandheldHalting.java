cd4431f8fcdf0d07b71d4928c5a841b258359ce1
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-12-09 01:12:41 +0100

    Solution for first challenge of day 8.

---
+++ src/com/adventofcode/day8/HandheldHalting.java
@@ -0,0 +1,61 @@
+package com.adventofcode.day8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.adventofcode.day8.Instruction.Operation.acc;
+
+public class HandheldHalting {
+    private static final String BOOT_CODE = "resources/boot_code_day_8.txt";
+    private static final int OFFSET_NEXT_INSTRUCTION = 1;
+    private static List<Instruction> instructionList = new ArrayList<>();
+    private static List<Integer> instructionPosition = new ArrayList<>();
+    private static int accumulator = 0;
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader(BOOT_CODE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                instructionList.add(readInstruction(line));
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        readBootCode(0);
+        System.out.println("Accumulator: " + accumulator);
+    }
+
+    private static Instruction readInstruction(String line) {
+        Instruction instruction = new Instruction();
+        String [] instructionParts = line.split(" ");
+        instruction.setOperation(Instruction.Operation.valueOf(instructionParts[0]));
+        if (acc == instruction.getOperation()
+            || Instruction.Operation.jmp == instruction.getOperation()) {
+            instruction.setOffset(Integer.valueOf(instructionParts[1]));
+        } else {
+            instruction.setOffset(OFFSET_NEXT_INSTRUCTION); // next instruction
+        }
+        System.out.println(instruction);
+        return instruction;
+    }
+
+    private static void readBootCode(int position) {
+        while (!instructionPosition.contains(position)) {
+            instructionPosition.add(position);
+            position += processInstruction(instructionList.get(position));
+        }
+    }
+
+    private static int processInstruction(Instruction instruction) {
+        switch (instruction.getOperation()) {
+            case acc:
+                accumulator += instruction.getOffset();
+                return OFFSET_NEXT_INSTRUCTION;
+            default:
+                return instruction.getOffset();
+        }
+    }
+}
